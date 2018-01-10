30473cdea804ae979c5c2c35e79279050dfc32f4
Oluwakemi Fasae <kemifasae@yahoo.co.uk>
2018-01-10 12:58:25 +0100

    write stringbuilder function

---
+++ stringBuilder/lib/strBuilder.js
@@ -0,0 +1,46 @@
+module.exports =  class stringBuilder{
+  constructor(str){
+    this.str = [];
+  }
+
+  isEmpty(newstr){
+    if (newstr.length === 0){
+      return true
+    }
+    return false
+  }
+
+  put(newStr){
+    this.str.push(newStr)
+    return this
+  }
+
+  insert(index, new_str){
+    let len = this.str.length
+
+    if(index < 0){
+      throw new Error('impossible index')
+    }
+    if(index>len){
+      this.put(new_str)
+      return this
+    }
+
+    //let arrStr = this.str.split(' ')
+    this.str.splice(index, 0, new_str);
+    //this.str = arrStr.join(' ')
+    return this
+  }
+
+  empty(){
+    this.str = []
+    return this
+  }
+
+  build(){
+    let finString = this.str.join(' ')
+    if(this.isEmpty(finString)) return('empty string')
+    return finString
+  }
+
+}
